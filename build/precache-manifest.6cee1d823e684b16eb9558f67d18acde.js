0fb464260ff8a33a4b853e6f5486d7ae0bf80152
abdullahyaqoob <65088172+abdullahyaqoob@users.noreply.github.com>
2022-03-01 13:34:29 +0500

    Add files via upload

---
+++ build/precache-manifest.6cee1d823e684b16eb9558f67d18acde.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "f449ac205a540da407b9",
+    "url": "/static/css/main.2033124a.chunk.css"
+  },
+  {
+    "revision": "f449ac205a540da407b9",
+    "url": "/static/js/main.f449ac20.chunk.js"
+  },
+  {
+    "revision": "9d21bead8c1fbc9d16c7",
+    "url": "/static/css/1.5e8e32fc.chunk.css"
+  },
+  {
+    "revision": "9d21bead8c1fbc9d16c7",
+    "url": "/static/js/1.9d21bead.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "78c9cc6fb908bb5a09b40e0569fdcf8c",
+    "url": "/static/media/footerLogo.78c9cc6f.png"
+  },
+  {
+    "revision": "a520b8897c3fee27a5b03a4eb326691f",
+    "url": "/static/media/laExchange.a520b889.png"
+  },
+  {
+    "revision": "b1a0ca6a2f0b2ad1e8701564fe6b21fa",
+    "url": "/static/media/graphDesktop.b1a0ca6a.png"
+  },
+  {
+    "revision": "6b1844e73497fd609ec8cb34e1a28f3b",
+    "url": "/static/media/graphArtificalView.6b1844e7.png"
+  },
+  {
+    "revision": "a233a523da9b145fba9cd29e1c59a776",
+    "url": "/static/media/binanceSmartChain.a233a523.png"
+  },
+  {
+    "revision": "d343595b34c1bd78b996b02f3301b98b",
+    "url": "/static/media/polygonIcon.d343595b.png"
+  },
+  {
+    "revision": "d3bdeb52b391a65663cbae626deb7fb5",
+    "url": "/static/media/metamaskIcon.d3bdeb52.png"
+  },
+  {
+    "revision": "96f3803ed9fc6445b127ea0e448ebf64",
+    "url": "/index.html"
+  }
+];
